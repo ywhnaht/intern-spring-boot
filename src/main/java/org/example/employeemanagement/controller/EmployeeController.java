4b3450b8c4a70f942d6026a1d37c412bde0155fb
ywhnaht <hothanhhuy24062004@gmail.com>
2025-11-04 14:13:19 +0700

    completed module 4: spring boot + database (jpa)

--- src/main/java/org/example/employeemanagement/controller/EmployeeController.java
+++ src/main/java/org/example/employeemanagement/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package org.example.employeemanagement.controller;
 
 import org.example.employeemanagement.entity.Employee;
+import org.example.employeemanagement.service.EmployeeService;
 import org.example.employeemanagement.service.UtilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,25 +15,54 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/employee")
 public class EmployeeController {
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final EmployeeService employeeService;
 
-    @Autowired
-    private UtilityService utilityService;
-
-    private final List<Employee> employeeList = new ArrayList<>();
-
-    @PostMapping
-    public ResponseEntity<Employee> createEmployee(@RequestBody Employee newEmployee) {
-        String employeeId = utilityService.generateAutoId();
-        newEmployee.setId(employeeId);
-        newEmployee.setPassword(passwordEncoder.encode(newEmployee.getPassword()));
-        employeeList.add(newEmployee);
-        return ResponseEntity.ok(newEmployee);
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
-    @GetMapping
+    @PostMapping("/create")
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee newEmployee) {
+        Employee employee = employeeService.createEmployee(newEmployee);
+        return ResponseEntity.ok(employee);
+    }
+
+    @GetMapping("/all")
     public  ResponseEntity<List<Employee>> getAllEmployees() {
-        return ResponseEntity.ok(employeeList);
+        List<Employee> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee updateEmployee) {
+        Employee employee = employeeService.updateEmployee(id, updateEmployee);
+        return ResponseEntity.ok(employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.ok("Delete Employee successfully");
+    }
+
+    @GetMapping()
+    public ResponseEntity<Employee> getEmployeeByName(@RequestParam String employeeName) {
+        Employee employee = employeeService.getEmployeeByName(employeeName);
+        if (employee == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(employee);
+    }
+
+    @GetMapping("/department")
+    public ResponseEntity<List<Employee>> getEmployeeByDepartment(@RequestParam String departmentName) {
+        List<Employee> employees = employeeService.getEmployeeByDepartment(departmentName);
+        return ResponseEntity.ok(employees);
     }
 }
--- src/main/java/org/example/employeemanagement/entity/Department.java
+++ src/main/java/org/example/employeemanagement/entity/Department.java
@@ -1,27 +1,26 @@
 package org.example.employeemanagement.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.util.List;
+
 @Entity
-@Table(name = "employee")
+@Table(name = "department")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class Employee {
+public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     String name;
-    String email;
+    String description;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "department_id", nullable = false)
-    Department department;
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
+    List<Employee> employees;
 }
